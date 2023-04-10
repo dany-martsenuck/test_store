5a3a7912bc87f14c559b3d54f2c009a8d65f6cb5
dany-martsenuck <62779649+dany-martsenuck@users.noreply.github.com>
2023-04-10 06:56:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,42 +3,42 @@ const spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4uRg4A
 const productsContainer = document.querySelector('#products .row');
 
 async function fetchProducts() {
-  const response = await fetch(spreadsheetUrl);
-  const data = await response.text();
-  const products = Papa.parse(data, { header: true }).data;
-  return products;
+   const response = await fetch(spreadsheetUrl);
+   const data = await response.text();
+   const products = Papa.parse(data, { header: true }).data;
+   return products;
 }
 
 function createProductCard(product) {
-  const card = document.createElement('div');
-  card.classList.add('card');
+   const card = document.createElement('div');
+   card.classList.add('card');
 
-  const img = document.createElement('img');
-  img.src = product['Product Image'];
-  img.alt = product['Product Name'];
-  card.appendChild(img);
+   const img = document.createElement('img');
+   img.src = product['Product Image'];
+   img.alt = product['Product Name'];
+   card.appendChild(img);
 
-  const h2 = document.createElement('h2');
-  h2.textContent = product['Product Name'];
-  card.appendChild(h2);
+   const h2 = document.createElement('h2');
+   h2.textContent = product['Product Name'];
+   card.appendChild(h2);
 
-  const p = document.createElement('p');
-  p.textContent = `$${product['Price']}`;
-  card.appendChild(p);
+   const p = document.createElement('p');
+   p.textContent = `$${product['Price']}`;
+   card.appendChild(p);
 
-  const button = document.createElement('button');
-  button.textContent = 'Add to Cart';
-  card.appendChild(button);
+   const button = document.createElement('button');
+   button.textContent = 'Add to Cart';
+   card.appendChild(button);
 
-  return card;
+   return card;
 }
 
 async function renderProducts() {
-  const products = await fetchProducts();
-  products.forEach(product => {
-    const card = createProductCard(product);
-    productsContainer.appendChild(card);
-  });
+   const products = await fetchProducts();
+   products.forEach(product => {
+      const card = createProductCard(product);
+      productsContainer.appendChild(card);
+   });
 }
 
 renderProducts();
